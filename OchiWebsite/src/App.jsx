05b1cf5234600016bac2e9da3b5d4512dea18821
Nikhil-Jalandhra <jneek36@gmail.com>
2024-02-15 12:57:50 +0530

    Creating Ochi award wining Website clone With the help of YouTube

---
+++ OchiWebsite/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div>
+      <div className='w-full min-h-screen text-white bg-zinc-900'>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
