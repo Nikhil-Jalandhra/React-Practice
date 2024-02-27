e9aafea9b4e7cbf3f165ef157c0cc8fea6f1a04b
Nikhil-Jalandhra <jneek36@gmail.com>
2024-02-27 20:18:12 +0530

    Adding component to this app

--- FullFledged/src/App.jsx
+++ FullFledged/src/App.jsx
@@ -1,6 +1,17 @@
-import './App.css'
+import { useState, useEffect } from 'react';
+import { UseDispatch } from 'react-redux';
+import './App.css';
+import authService from "./appwrite/auth"
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = UseDispatch();
+
+  useEffect(()=> {
+    authService.getCurrentUser()
+    .then((useData)=>{})
+    .finally()
+  }, [])
 
   return (
     <>
