51859539b820311b7040f60a1d8e83d78556b8ca
Nikhil-Jalandhra <jneek36@gmail.com>
2024-02-28 21:48:53 +0530

    Functioning in project

--- FullFledged/src/App.jsx
+++ FullFledged/src/App.jsx
@@ -2,22 +2,36 @@ import { useState, useEffect } from 'react';
 import { UseDispatch } from 'react-redux';
 import './App.css';
 import authService from "./appwrite/auth"
+import {login, logout} from './store/authSlice'
+import { Header, Footer } from './components'
 
 function App() {
+
   const [loading, setLoading] = useState(true);
   const dispatch = UseDispatch();
 
   useEffect(()=> {
     authService.getCurrentUser()
-    .then((useData)=>{})
-    .finally()
+    .then((useData)=>{
+      if (useData) {
+        dispatch(login({useData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
   }, [])
 
-  return (
-    <>
-      <h1>Hitesh Choudhary</h1>
-    </>
-  )
+  return !loading ? (
+    <div className=' min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <Footer />
+        <main></main>
+      </div>
+    </div>
+  ) : null
+
 }
 
 export default App
