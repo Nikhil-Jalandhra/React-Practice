27318c64531999a56fac8cf62e07851fbcc53cd4
Nikhil-Jalandhra <jneek36@gmail.com>
2024-03-01 21:23:37 +0530

    working on components in the project

--- FullFledged/src/components/header/Header.jsx
+++ FullFledged/src/components/header/Header.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import {Container, Logo, LogoOutButton} from '../index'
+import { Link } from 'react-router-dom';
+import { UseSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
   return (
--- FullFledged/src/components/index.js
+++ FullFledged/src/components/index.js
@@ -1,7 +1,13 @@
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoOutButton from "./header/LogoOutButton";
 
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoOutButton
 }
