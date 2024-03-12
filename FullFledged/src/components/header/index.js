434b87b57486d70574c79d57e54d96316088387e
Nikhil-Jalandhra <jneek36@gmail.com>
2024-03-12 21:55:05 +0530

    Completed the function of RTE(Rich TEXT EDITOR) component

--- FullFledged/src/components/header/index.js
+++ FullFledged/src/components/header/index.js
@@ -3,11 +3,13 @@ import Footer from "../footer/Footer";
 import Container from "../container/Container";
 import Logo from "./Logo";
 import LogoOutButton from "./LogoOutButton";
+import RTE from "../RTE";
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoOutButton
+    LogoOutButton,
+    RTE
 }
