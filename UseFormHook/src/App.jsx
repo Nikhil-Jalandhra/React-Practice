47f600895a5855f6d8586c7490c7922fa8112b23
Nikhil-Jalandhra <jneek36@gmail.com>
2024-03-13 21:54:14 +0530

    Learning React Hook (Form) Part-1

--- UseFormHook/src/App.jsx
+++ UseFormHook/src/App.jsx
@@ -1,33 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useForm } from 'react-hook-form';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const {register, handleSubmit, errors} = useForm()
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='w-full bg-gray-800 h-screen p-[100px]'>
+      <div className='container border m-auto  h-auto px-5 py-7 w-[450px]  border-slate-950 bg-blue-600 rounded-lg'>
+        <form>
+          <h1 className=' text-[40px] font-semibold ml-[40px]'>Registration Form</h1>
+          <div className='ui divider w-[400px] border border-black m-3'></div>
+            <div className='ui form flex flex-col items-center'>
+              <div className='field flex flex-col'>
+                <label className='p-2 text-[20px] font-sans font-semibold'>User Name</label>
+                <input type="text" name='username' placeholder='Username' className='w-[400px] rounded-sm px-3 py-2' ref={register}/>
+              </div>
+              <div className='field flex flex-col'>
+                <label className='p-2 text-[20px] font-sans font-semibold'>Email</label>
+                <input type="email" name='email' placeholder='Email' className='w-[400px] rounded-sm px-3 py-2' ref={register}/>
+              </div>
+              <div className='field flex flex-col'>
+                <label className='p-2 text-[20px] font-sans font-semibold'>Password</label>
+                <input type="password" name='password' placeholder='Password' className='w-[400px] rounded-sm px-3 py-2' ref={register}/>
+              </div>
+              <button className='w-[200px] h-[30px] mt-[25px] bg-white'>Submit</button>
+            </div>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
