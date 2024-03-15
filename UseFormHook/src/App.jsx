48acebcc9300e09d3f1f8ccd3ae10f6585923c18
Nikhil-Jalandhra <jneek36@gmail.com>
2024-03-15 12:40:40 +0530

    Learning React Hook (Form) Part-3

--- UseFormHook/src/App.jsx
+++ UseFormHook/src/App.jsx
@@ -3,7 +3,8 @@ import { useForm } from 'react-hook-form';
 
 function App() {
 
-  const {register, handleSubmit, errors} = useForm();
+  const {register, handleSubmit,
+     formState: {errors}, } = useForm();
   const [userInfo, setUserInfo] = useState();
   const onSubmit = (data) =>{
     setUserInfo(data)
@@ -12,8 +13,8 @@ function App() {
   
   return (
     <>
-    <div className='w-full bg-gray-800 h-auto p-[100px]'>
-      <div className='container border m-auto  h-auto px-5 py-7 w-[450px]  border-slate-950 bg-blue-600 rounded-lg'>
+    <div className='w-full bg-gray-800 h-screen p-[100px]'>
+      <div className='container border m-auto  h-auto px-5 py-7 w-[450px]  border-slate-950 bg-blue-500 rounded-lg'>
     <pre> {JSON.stringify(userInfo, undefined, 2)}</pre>
         <form onSubmit={handleSubmit(onSubmit)}>
           <h1 className=' text-[40px] font-semibold ml-[40px]'>Registration Form</h1>
@@ -21,15 +22,18 @@ function App() {
             <div className='ui form flex flex-col items-center'>
               <div className='field flex flex-col'>
                 <label className='p-2 text-[20px] font-sans font-semibold'>User Name</label>
-                <input type="text" name='username' placeholder='Username' className='w-[400px] rounded-sm px-3 py-2' {...register("UserName")} />
+                <input type="text" name='username' placeholder='Username' className='w-[400px] rounded-sm px-3 py-2' {...register("userName", {required: true})} />
+                {errors.userName && <p className=' text-red-700 pt-1 font-bold' >The User Name is required</p>}
               </div>
               <div className='field flex flex-col'>
                 <label className='p-2 text-[20px] font-sans font-semibold'>Email</label>
-                <input type="email" name='email' placeholder='Email' className='w-[400px] rounded-sm px-3 py-2'  {...register("Email")}/>
+                <input type="email" name='email' placeholder='Email' className='w-[400px] rounded-sm px-3 py-2'  {...register("Email", {required: true})}/>
+                {errors.Email && <p className=' text-red-700 pt-1 font-bold' >The Email is required</p>}
               </div>
               <div className='field flex flex-col'>
                 <label className='p-2 text-[20px] font-sans font-semibold'>Password</label>
-                <input type="password" name='password' placeholder='Password' className='w-[400px] rounded-sm px-3 py-2' {...register("Password")}/>
+                <input type="password" name='password' placeholder='Password' className='w-[400px] rounded-sm px-3 py-2' {...register("Password", {required: true})}/>
+                {errors.Password && <p className=' text-red-700 pt-1 font-bold' >The Password is required</p>}
               </div>
               <button className='w-[200px] h-[30px] mt-[25px] bg-white  rounded-sm text-25px font-semibold'>Submit</button>
             </div>
