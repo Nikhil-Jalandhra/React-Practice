12a5413267c3e2e21241875eb84da582417678e7
Nikhil-Jalandhra <jneek36@gmail.com>
2024-04-04 20:51:28 +0530

    Creating Appwrite authentication service

--- Appwrite-Authentication/src/App.jsx
+++ Appwrite-Authentication/src/App.jsx
@@ -2,10 +2,13 @@ import './App.css'
 
 function App() {
 
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+
+
   return (
     <>
-      Hello
-    </>
+    <div className='w-full h-screen bg-gray-700 flex justify-center items-center text-[50px] font-bold text-white'>Learning Authentication 🤩</div>
+    </> 
   )
 }
 
---
+++ Appwrite-Authentication/src/appwrite/auth.jsx
@@ -0,0 +1,7 @@
+import {Account, Client, ID} from 'appwrite'
+
+export class AuthService{
+
+}
+
+export default AuthService
