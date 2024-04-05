9c9aa984e722062284f908e03fcc80a8eb047c81
Nikhil-Jalandhra <jneek36@gmail.com>
2024-04-05 21:55:30 +0530

    login feature

--- Appwrite-Authentication/src/appwrite/auth.jsx
+++ Appwrite-Authentication/src/appwrite/auth.jsx
@@ -1,7 +1,31 @@
 import {Account, Client, ID} from 'appwrite'
+import conf from '../envImport/conf';
 
 export class AuthService{
+     client = new Client()
+     account;
 
+    constructor(){
+        this.client()
+       .setEndpoint(conf.appwriteUrl)        
+       .setProject(conf.appwriteProjectId);
+       this.account = new Account(this.client)
+    }
+
+    async creatAccount({email, password, name}){
+        try {
+        const userAccount =  this.account.create(ID.unique(), email, password, name)
+        if (userAccount) {
+            
+        }else{
+            return
+        }
+        } catch (error) {
+            console.log(error)
+        }
+    }
 }
 
-export default AuthService
+ const authService = new AuthService();
+
+export default authService
