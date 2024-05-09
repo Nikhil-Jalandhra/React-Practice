c18d0fb14b629804a70861426558467710482a1f
Nikhil-Jalandhra <jneek36@gmail.com>
2024-05-09 22:52:42 +0530

    working on responsive navbar

--- DisneyClone/src/Components/Header.jsx
+++ DisneyClone/src/Components/Header.jsx
@@ -44,21 +44,42 @@ function Header() {
     ]
 
   return (
-    <div className=' flex w-full h-[80px] items-center text-white'>
-            <img src={logo} alt="Logo" className='h-[50px] mx-[35px]' />
-        <div className='flex'>
+    <div className=' flex w-full h-[80px] text-white'>
+        <div className='h-full w-[250px] flex items-center pl-[40px] mr-[25px]'>
+            <img src={logo} alt="Logo" className='w-[70px]' />
+        </div>
+
+        {/* Following Div For Larger Screen */}
+
+        <div className='hidden lg:flex w-full h-full items-center justify-between'>
             {menue.map((item,index)=>(
                 <a key={index} href={item.slug}>
-                    <p className=' ml-[50px] flex items-center
-                       font-bold text-[18px] cursor-pointer border-[#16181f]
-                       border-b-[2px] hover:border-white'>
-                        <span className='mr-[10px] text-16px]'>{item.icon}</span>{item.name}
+                    <p className='flex items-center
+                       font-semibold text-[18px] cursor-pointer border-[#16181f]
+                       border-b-[2px] hover:border-white mr-[20px]'>
+                        <span className='mr-[5px] text-16px]'>{item.icon}</span>{item.name}
                     </p>
                 </a>
                 ))}
         </div>
-        <div className='w-[45px]  rounded-full overflow-hidden ml-[480px]'>
-            <img src={avatar} alt="" className='' />
+
+        {/* Following Div For Medium Screen */}
+
+        <div className=' lg:hidden hidden md:flex h-full items-center w-full justify-between'>
+            {menue.map((item,index)=>(
+                <a key={index} href={item.slug}>
+                    <p className='flex items-center
+                    cursor-pointer'>
+                        <span className='mr-[5px] text-16px]'>{item.icon}</span>
+                    </p>
+                </a>
+                ))}
+        </div>
+
+        <div className='lg:w-1/2 md:w-1/2 w-full h-full flex items-center justify-end pr-[20px]'>
+            <div className='w-[45px]  rounded-full overflow-hidden'>
+                <img src={avatar} alt="" className='' />
+            </div>
         </div>
     </div>
   );
