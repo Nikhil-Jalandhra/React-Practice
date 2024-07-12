85b6fcce200e8c2dea9d563c364b6c490a5ee9c1
Nikhil-Jalandhra <jneek36@gmail.com>
2024-07-12 21:30:47 +0530

    app write review

--- Appwrite-Authentication/src/App.jsx
+++ Appwrite-Authentication/src/App.jsx
@@ -2,9 +2,6 @@ import './App.css'
 
 function App() {
 
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-
-
   return (
     <>
     <div className='w-full h-screen bg-gray-700 flex justify-center items-center text-[50px] font-bold text-white'>Learning Authentication 🤩</div>
